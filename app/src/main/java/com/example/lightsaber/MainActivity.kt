77813068ce8305416d685ff373bd12a84180252c
GyarmatiTamas <gyarmatitamas01@gmail.com>
2021-10-23 16:37:33 +0200

    feat: color choose image added

--- app/src/main/java/com/example/lightsaber/MainActivity.kt
+++ app/src/main/java/com/example/lightsaber/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import androidx.core.view.isVisible
 import java.lang.Math.abs
@@ -18,25 +20,46 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     lateinit var img: ImageView
     lateinit var hilt: ImageView
+    lateinit var color: ImageView
+
     lateinit var onSound: MediaPlayer
     lateinit var startSound: MediaPlayer
     lateinit var sensorManager: SensorManager
     lateinit var leftSwingPlayer: MediaPlayer
     lateinit var rightSwingPlayer: MediaPlayer
 
+    lateinit var stb: Animation
+    lateinit var light_on: Animation
+    lateinit var light_off: Animation
+    lateinit var color_on: Animation
+    lateinit var color_off: Animation
+
     lateinit var endSound: MediaPlayer
     var visible = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        hilt = findViewById(R.id.hilt)
+        color = findViewById(R.id.color)
+
+        stb = AnimationUtils.loadAnimation(this,R.anim.stb)
+        light_on = AnimationUtils.loadAnimation(this,R.anim.light_on)
+        light_off = AnimationUtils.loadAnimation(this,R.anim.light_off)
+        color_on = AnimationUtils.loadAnimation(this,R.anim.color_on)
+        color_off = AnimationUtils.loadAnimation(this,R.anim.color_off)
 
         onSound = MediaPlayer.create(this,R.raw.on_sound)
         startSound = MediaPlayer.create(this,R.raw.start_sound)
         leftSwingPlayer = MediaPlayer.create(this, R.raw.left_swing)
         rightSwingPlayer = MediaPlayer.create(this, R.raw.right_swing)
         endSound = MediaPlayer.create(this,R.raw.end_sound)
+
+        hilt.startAnimation(stb)
+        color.startAnimation(stb)
 
         setUpSensors()
     }
@@ -55,21 +78,28 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     fun hiltPushed(view: View) {
         img = findViewById(R.id.lightBlade)
-        hilt = findViewById(R.id.hilt)
+
+
         if (visible == 0)
         {
+            img.startAnimation(light_on)
+            color.startAnimation(color_off)
             startSound.start()
             onSound.start()
             onSound.isLooping = true
             visible = 1
             img.visibility = View.VISIBLE
+            color.visibility = View.INVISIBLE
         }
         else
         {
+            img.startAnimation(light_off)
+            color.startAnimation(color_on)
             endSound.start()
             onSound.pause()
             visible = 0
             img.visibility = View.INVISIBLE
+            color.visibility = View.VISIBLE
         }
     }
 
@@ -86,11 +116,14 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
                 rightSwingPlayer.start()
             }
 
-
         }
     }
 
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
+    }
+
+    fun onColorClicked(view: View) {
 
     }
 
